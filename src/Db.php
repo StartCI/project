47f1742a4d6371e595457c5f308a6207d4717682
Felipe Azambuja <felipe@newbgp.com.br>
2023-04-10 13:18:59 -0300

    1.1.1

--- src/Db.php
+++ src/Db.php
@@ -2,6 +2,7 @@
 
 namespace CodeIgniter\Startci;
 
+use CodeIgniter\Database\Config;
 use CodeIgniter\Database\ConnectionInterface;
 
 /**
@@ -29,6 +30,78 @@ class Db
         if (method_exists($this->con, $name))
             return $this->con->{$name}(...$params);
         return null;
+    }
+
+    function whereRaw($cond)
+    {
+        $this->con->where($cond, null, false);
+        return $this->con;
+    }
+
+    /**
+     * Sets a test mode status.
+     *
+     * @param boolean $type Mode to set
+     *
+     * @return mixed
+     */
+    public function rs(string $type = 'object')
+    {
+        return $this->result($type);
+    }
+
+    /**
+     * Sets a test mode status.
+     *
+     * @param boolean $type Mode to set
+     *
+     * @return mixed
+     */
+    public function result(string $type = 'object')
+    {
+        return $this->con->get()->getResult($type);
+    }
+
+    /**
+     * Sets a test mode status.
+     *
+     * @param boolean $type Mode to set
+     *
+     * @return mixed
+     */
+    public function first(string $type = 'object')
+    {
+        return $this->con->get()->getFirstRow($type);
+    }
+
+    /**
+     * Sets a test mode status.
+     *
+     * @param boolean $type Mode to set
+     *
+     * @return mixed
+     */
+    public function last(string $type = 'object')
+    {
+        return $this->con->get()->getLastRow($type);
+    }
+
+    /**
+     * Sets a test mode status.
+     *
+     * @param boolean $mode Mode to set
+     *
+     * @return BaseBuilder
+     */
+    function def($values = [])
+    {
+        $table = $this->con->tableName;
+        $db = $this->con->db();
+        $v = [];
+        foreach ($db->getFieldNames($table) as $key => $value) {
+            $v[$value] = (!isset($values[$value])) ? null : $values[$value];
+        }
+        return (object) $v;
     }
     
 }
