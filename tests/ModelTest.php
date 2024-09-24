ca2d0a6cac705af84d1cd8237429d6879e9680b9
Felipe Nunes Azambuja <felipe@newbgp.com.br>
2024-09-23 22:22:27 -0300

    up

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -11,24 +11,24 @@ beforeEach(function () {});
 
 test('create', function () {
     $dbs = [
-        db([
-            'DBDriver' => 'MySQLi',
-            'hostname' => '127.0.0.1',
-            'database' => 'startci',
-            'username' => 'root',
-            'password' => '123',
-            'port' => 3306,
-            'charset' => 'utf8',
-            'DBCollat' => 'utf8_general_ci',
-        ]),
-        db([
-            'DBDriver' => 'Postgre',
-            'hostname' => 'localhost',
-            'database' => 'startci',
-            'username' => 'startci',
-            'password' => '3af8601b46ab39f0',
-            'charset'  => 'utf8',
-        ]),
+        // db([
+        //     'DBDriver' => 'MySQLi',
+        //     'hostname' => '127.0.0.1',
+        //     'database' => 'startci',
+        //     'username' => 'root',
+        //     'password' => '123',
+        //     'port' => 3306,
+        //     'charset' => 'utf8',
+        //     'DBCollat' => 'utf8_general_ci',
+        // ]),
+        // db([
+        //     'DBDriver' => 'Postgre',
+        //     'hostname' => 'localhost',
+        //     'database' => 'startci',
+        //     'username' => 'startci',
+        //     'password' => '3af8601b46ab39f0',
+        //     'charset'  => 'utf8',
+        // ]),
         db([
             'DBDriver' => 'SQLite3',
             'hostname' => 'db.sqlite',
@@ -40,13 +40,87 @@ test('create', function () {
         cache()->clean();
         $model = new \App\Models\Usuarios\Clientes($db);
         $model->create();
-        $model->nome = 'felipe';
-        $model->save();
-        $model->nome = 'felipe2';
-        $model->save();
-        $json = $model->toJson();
+        // $model->nome = 'felipe';
+        // $model->save();
+        // $model->nome = 'felipe2';
+        // $model->save();
+        // $json = $model->toJson();
+        $tables = $db->listTables();
+        expect($tables)->toBeArray();
+        expect($tables)->toHaveCount(2);
+        expect($tables)->toContain('usuarios');
+        expect($tables)->toContain('clientes');
+        // xdebug_break();
     }
 });
+
+test('save', function () {
+
+    $dbs = [
+        // db([
+        //     'DBDriver' => 'MySQLi',
+        //     'hostname' => '127.0.0.1',
+        //     'database' => 'startci',
+        //     'username' => 'root',
+        //     'password' => '123',
+        //     'port' => 3306,
+        //     'charset' => 'utf8',
+        //     'DBCollat' => 'utf8_general_ci',
+        // ]),
+        // db([
+        //     'DBDriver' => 'Postgre',
+        //     'hostname' => 'localhost',
+        //     'database' => 'startci',
+        //     'username' => 'startci',
+        //     'password' => '3af8601b46ab39f0',
+        //     'charset'  => 'utf8',
+        // ]),
+        db([
+            'DBDriver' => 'SQLite3',
+            'hostname' => 'db.sqlite',
+            'database' => 'db.sqlite',
+        ])
+    ];
+    foreach ($dbs as $key => $db) {
+        cache()->clean();
+        $model = new \App\Models\Usuarios\Clientes($db);
+    }
+});
+
+test('delete', function () {
+    $dbs = [
+        // db([
+        //     'DBDriver' => 'MySQLi',
+        //     'hostname' => '127.0.0.1',
+        //     'database' => 'startci',
+        //     'username' => 'root',
+        //     'password' => '123',
+        //     'port' => 3306,
+        //     'charset' => 'utf8',
+        //     'DBCollat' => 'utf8_general_ci',
+        // ]),
+        // db([
+        //     'DBDriver' => 'Postgre',
+        //     'hostname' => 'localhost',
+        //     'database' => 'startci',
+        //     'username' => 'startci',
+        //     'password' => '3af8601b46ab39f0',
+        //     'charset'  => 'utf8',
+        // ]),
+        db([
+            'DBDriver' => 'SQLite3',
+            'hostname' => 'db.sqlite',
+            'database' => 'db.sqlite',
+        ])
+    ];
+    foreach ($dbs as $key => $db) {
+        $model = new \App\Models\Usuarios\Clientes($db);
+        $model->where('id', '1')->delete();//null
+    }
+});
+
+test('select', function () {});
+
 
 test('up', function () {
     // Services::commands()->run('startci:orm', ['up']);
@@ -57,12 +131,3 @@ test('up', function () {
 
 test('seed', function () {});
 
-test('connection', function () {});
-
-test('insert', function () {});
-
-test('update', function () {});
-
-test('delete', function () {});
-
-test('select', function () {});
