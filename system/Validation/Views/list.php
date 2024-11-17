e93f7bb59668bc5ac6744dac40eb9c5e0d97f6c8
Felipe Nunes Azambuja <felipe@newbgp.com.br>
2024-11-17 01:25:34 -0300

    up

--- system/Validation/Views/list.php
+++
@@ -1,9 +0,0 @@
-<?php if (isset($errors) && $errors !== []) : ?>
-	<div class="errors" role="alert">
-		<ul>
-		<?php foreach ($errors as $error) : ?>
-			<li><?= esc($error) ?></li>
-		<?php endforeach ?>
-		</ul>
-	</div>
-<?php endif ?>
